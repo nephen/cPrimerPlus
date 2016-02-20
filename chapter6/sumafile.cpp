23ff676b42917495de8b84760e4f1dc6029c5037
nephen <18682441907@163.com>
2016-02-20 11:55:16 +0800

    chapter6: finished

---
+++ chapter6/sumafile.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+const int SIZE = 60;
+
+/**
+ * input scores.txt
+ * @param  argc [description]
+ * @param  argv [description]
+ * @return      [description]
+ */
+int main(int argc, char const *argv[])
+{
+	using namespace std;
+	char filename[SIZE];
+	ifstream inFile;
+
+	cout << "Enter name of data file: ";
+	cin.getline(filename, SIZE);
+	inFile.open(filename);
+	if (!inFile.is_open())
+	{
+		cout << "Could't open the file " << filename << endl;
+		cout << "Program terminate.\n";
+		exit(EXIT_FAILURE);
+	}
+	double value;
+	double sum = 0.0;
+	int count = 0;
+
+	inFile >> value;
+	while (inFile.good())
+	{
+		++count;
+		sum += value;
+		inFile >> value;
+	}
+	if (inFile.eof())
+		cout << "End of file reached.\n";
+	else if (inFile.fail())
+		cout << "Input terminated by data mismatch.\n";
+	else
+		cout << "Input terminated for unknown reason.\n";
+	if (count == 0)
+		cout << "No data processed.\n";
+	else
+	{
+		cout << "Items read: " << count << endl;
+		cout << "Sum: " << sum << endl;
+		cout << "Average: " << sum / count << endl;
+	}
+	inFile.close();
+	return 0;
+}
